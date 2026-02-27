ffbdfb5b1cff4691b77c629e953f3fc14a250465
Samarth <atreyasamarth@gmail.com>
2026-02-27 16:39:32 +0530

    Create ProjectsPage component for project listing

---
+++ frontend/src/routes/ProjectsPage.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage() {
+  return (
+    <div className="projects-page">
+      <h1>My Projects</h1>
+      {/* TODO: List/load/save projects */}
+    </div>
+  );
+}
