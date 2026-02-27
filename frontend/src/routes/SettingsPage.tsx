4ad477ce0e8c8afce51c5e05856659ce77cf5f32
Samarth <atreyasamarth@gmail.com>
2026-02-27 16:40:02 +0530

    Add initial SettingsPage component

---
+++ frontend/src/routes/SettingsPage.tsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+  return (
+    <div className="settings-page">
+      <h1>Settings</h1>
+      {/* TODO: Board defaults, theme, serial port preferences */}
+    </div>
+  );
+}
